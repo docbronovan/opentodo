60bc4ef93d5c9a02a4d60372deb99470901b3145
docbronovan <brockdonovan@gmail.com>
2015-08-19 21:38:58 -0400

    uninitialized constant ApiController

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   resources :items
   resources :lists
 
-  devise_for :users
+
   get 'welcome/index'
   get 'welcome/about'
   root 'welcome#index'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
